e4adbfb4950398c5f509cd4dbc4764deacad7e9b
sinaceylan <sina-ceylan@hotmail.com>
2022-07-09 15:31:28 +0300

    Initial Comments

--- app/src/main/java/com/sina/map/MapsActivity.java
+++ app/src/main/java/com/sina/map/MapsActivity.java
@@ -1,10 +1,17 @@
 package com.sina.map;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,71 +20,94 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sina.map.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    FloatingActionButton bottomsheet;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+                    mapFragment.getMapAsync(this);
 
-        Button buttonHybrid = findViewById(R.id.buttonHybrid);
-        buttonHybrid.setOnClickListener(new View.OnClickListener() {
+        bottomsheet = findViewById(R.id.botttom_sheet);
+
+        bottomsheet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            public void onClick(View v) {
+
+                final Dialog dialog = new Dialog(MapsActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.bottomsheetlayout);
+
+                Button darkButton = dialog.findViewById(R.id.dark_button);
+                Button hybridButton = dialog.findViewById(R.id.hybrid_button);
+                Button lightButton = dialog.findViewById(R.id.ligth_button);
+                Button satelitteButton = dialog.findViewById(R.id.satellite_button);
+
+                darkButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this,R.raw.map_style_dark_no_label));
+                        Toast.makeText(MapsActivity.this,"Dark is Clicked",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                lightButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this,R.raw.map_style_light_no_label));
+                        Toast.makeText(MapsActivity.this,"Light is Clicked",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                hybridButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                        Toast.makeText(MapsActivity.this,"Hybrid is Clicked",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                satelitteButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                        Toast.makeText(MapsActivity.this,"Satelitte is Clicked",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
             }
         });
-
-        Button buttonSattalite = findViewById(R.id.buttonSattelite);
-        buttonSattalite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-            }
-        });
-
-        Button buttonNightMode= findViewById(R.id.buttonNightMode);
-        buttonNightMode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this,R.raw.map_style_dark_no_label));
-            }
-        });
-
-        Button buttonLightMode = findViewById(R.id.buttonLightMode);
-        buttonLightMode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this,R.raw.map_style_light_no_label));
-
-            }
-        });
-
     }
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.getUiSettings().setRotateGesturesEnabled(true);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
