c27b457280e3bb115f99268a4c3acfd7428fe3fe
sinaceylan <sina-ceylan@hotmail.com>
2022-07-14 20:33:52 +0300

    Click Border/Text Color Done

--- app/src/main/java/com/sina/map/MapsActivity.java
+++ app/src/main/java/com/sina/map/MapsActivity.java
@@ -12,9 +12,8 @@ import android.view.Window;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
-
+import android.widget.TextView;
 import androidx.fragment.app.FragmentActivity;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,7 +23,6 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sina.map.databinding.ActivityMapsBinding;
-
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,7 +41,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     mapFragment.getMapAsync(this);
 
         bottomsheet = findViewById(R.id.botttom_sheet);
-
         bottomsheet.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("UseSwitchCompatOrMaterialCode")
             @Override
@@ -53,15 +50,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialog.setContentView(R.layout.bottomsheetlayout);
 
+                TextView dark_textView = dialog.findViewById(R.id.dark_textView);                   ///////////////////////
+                TextView light_textView = dialog.findViewById(R.id.light_textView);                 ///////////////////////
+                TextView hybrid_textView = dialog.findViewById(R.id.hybrid_textView);               ///////////////////////
+                TextView satelitte_textView = dialog.findViewById(R.id.satelitte_textView);         ///////////////////////
+
                 ImageButton darkButton = dialog.findViewById(R.id.dark_button);
                 ImageButton hybridButton = dialog.findViewById(R.id.hybrid_button);
                 ImageButton lightButton = dialog.findViewById(R.id.ligth_button);
                 ImageButton satelitteButton = dialog.findViewById(R.id.satellite_button);
                 Switch swbutton = dialog.findViewById(R.id.switch_button);
 
+                lightButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                hybridButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                satelitteButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                darkButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+
+                //Switch Button SharedPreferences
+                //SharedPreferences sharedPreferences=getSharedPreferences("save",MODE_PRIVATE);
+                //swbutton.setChecked(sharedPreferences.getBoolean("value",true));
+
                 darkButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        dark_textView.setText("Dark");                                              ///////////////////////
+                        dark_textView.setTextSize(14);                                              ///////////////////////
+                        dark_textView.setTextColor(Color.parseColor("#2979FF"));           ///////////////////////
+
+                        light_textView.setText("Light");                                            ///////////////////////
+                        light_textView.setTextSize(14);                                             ///////////////////////
+                        light_textView.setTextColor(Color.parseColor("#000000"));          ///////////////////////
+
+                        hybrid_textView.setText("Hybrid");                                          ///////////////////////
+                        hybrid_textView.setTextSize(14);                                            ///////////////////////
+                        hybrid_textView.setTextColor(Color.parseColor("#000000"));         ///////////////////////
+
+                        satelitte_textView.setText("Satelitte");                                    ///////////////////////
+                        satelitte_textView.setTextSize(14);                                         ///////////////////////
+                        satelitte_textView.setTextColor(Color.parseColor("#000000"));      ///////////////////////
+
+                        //For Changing Border Color
+                        lightButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        hybridButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        satelitteButton.setBackground(getResources().getDrawable(R.drawable.item_background));
 
                         darkButton.setBackground(getResources().getDrawable(R.drawable.on_item_select));
                         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -72,8 +103,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                                 if(isChecked){
+
+                                    //Switch Button SharedPreferences
+                                    //SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                                    //editor.putBoolean("value",true);
+                                    //editor.apply();
+                                    //swbutton.setChecked(true);
+
                                     mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this,R.raw.map_style_dark_with_labels));
                                 }else{
+
+                                    // Switch Button SharedPreferences
+                                    //SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                                    //editor.putBoolean("value",false);
+                                    //editor.apply();
+                                    //swbutton.setChecked(false);
+
                                     mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this,R.raw.map_style_dark_no_label));
                                 }
                             }
@@ -84,6 +129,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 lightButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
+                        dark_textView.setText("Dark");                                              ///////////////////////
+                        dark_textView.setTextSize(14);                                              ///////////////////////
+                        dark_textView.setTextColor(Color.parseColor("#000000"));           ///////////////////////
+
+                        light_textView.setText("Light");                                            ///////////////////////
+                        light_textView.setTextSize(14);                                             ///////////////////////
+                        light_textView.setTextColor(Color.parseColor("#2979FF"));          ///////////////////////
+
+                        hybrid_textView.setText("Hybrid");                                          ///////////////////////
+                        hybrid_textView.setTextSize(14);                                            ///////////////////////
+                        hybrid_textView.setTextColor(Color.parseColor("#000000"));         ///////////////////////
+
+                        satelitte_textView.setText("Satelitte");                                    ///////////////////////
+                        satelitte_textView.setTextSize(14);                                         ///////////////////////
+                        satelitte_textView.setTextColor(Color.parseColor("#000000"));      ///////////////////////
+
+                        //For Changing Border Color
+                        darkButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        hybridButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        satelitteButton.setBackground(getResources().getDrawable(R.drawable.item_background));
 
                         lightButton.setBackground(getResources().getDrawable(R.drawable.on_item_select));
                         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -105,8 +171,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 hybridButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        hybridButton.setBackground(getResources().getDrawable(R.drawable.on_item_select));
 
+                        dark_textView.setText("Dark");                                              ///////////////////////
+                        dark_textView.setTextSize(14);                                              ///////////////////////
+                        dark_textView.setTextColor(Color.parseColor("#000000"));           ///////////////////////
+
+                        light_textView.setText("Light");                                            ///////////////////////
+                        light_textView.setTextSize(14);                                             ///////////////////////
+                        light_textView.setTextColor(Color.parseColor("#000000"));          ///////////////////////
+
+                        hybrid_textView.setText("Hybrid");                                          ///////////////////////
+                        hybrid_textView.setTextSize(14);                                            ///////////////////////
+                        hybrid_textView.setTextColor(Color.parseColor("#2979FF"));         ///////////////////////
+
+                        satelitte_textView.setText("Satelitte");                                    ///////////////////////
+                        satelitte_textView.setTextSize(14);                                         ///////////////////////
+                        satelitte_textView.setTextColor(Color.parseColor("#000000"));      ///////////////////////
+
+                        //For Changing Border Color
+                        darkButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        lightButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        satelitteButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+
+                        hybridButton.setBackground(getResources().getDrawable(R.drawable.on_item_select));
                         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
                         //Toast.makeText(MapsActivity.this,"Hybrid is Clicked",Toast.LENGTH_SHORT).show();
                     }
@@ -115,8 +202,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 satelitteButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        satelitteButton.setBackground(getResources().getDrawable(R.drawable.on_item_select));
 
+                        dark_textView.setText("Dark");                                              ///////////////////////
+                        dark_textView.setTextSize(14);                                              ///////////////////////
+                        dark_textView.setTextColor(Color.parseColor("#000000"));           ///////////////////////
+
+                        light_textView.setText("Light");                                            ///////////////////////
+                        light_textView.setTextSize(14);                                             ///////////////////////
+                        light_textView.setTextColor(Color.parseColor("#000000"));          ///////////////////////
+
+                        hybrid_textView.setText("Hybrid");                                          ///////////////////////
+                        hybrid_textView.setTextSize(14);                                            ///////////////////////
+                        hybrid_textView.setTextColor(Color.parseColor("#000000"));         ///////////////////////
+
+                        satelitte_textView.setText("Satelitte");                                    ///////////////////////
+                        satelitte_textView.setTextSize(14);                                         ///////////////////////
+                        satelitte_textView.setTextColor(Color.parseColor("#2979FF"));      ///////////////////////
+
+                        //For Changing Border Color
+                        darkButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        hybridButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+                        lightButton.setBackground(getResources().getDrawable(R.drawable.item_background));
+
+                        satelitteButton.setBackground(getResources().getDrawable(R.drawable.on_item_select));
                         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                         //Toast.makeText(MapsActivity.this,"Satelitte is Clicked",Toast.LENGTH_SHORT).show();
                     }
@@ -129,17 +237,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 dialog.getWindow().setGravity(Gravity.BOTTOM);
             }
         });
-
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
-
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.getUiSettings().setRotateGesturesEnabled(true);
-
-        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Current Position"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
